9088fb8aece903407d01505fc5c82289e6e9484d
varshini <2116327@cognizant.com>
2024-03-17 10:21:02 +0530

    Comments removed

--- my-app/src/appStore.js
+++ my-app/src/appStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"; 
 import cardDetailsReducer from "./slices/cardHolderNameSlice";
-// import cardNumberReducer from "./slices/cardNumberSlice";
+
 
 
 export const store = configureStore({
